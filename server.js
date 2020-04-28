3b3e5014a86d91b810eea14901e2e2d8b439191a
Sumit <sumit.sth54@gmail.com>
2020-04-28 20:11:06 +0545

    fixing database connection

--- server.js
+++ server.js
@@ -9,19 +9,13 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-/*
 const db=knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
     ssl: true,
   }
-});*/
+});
 
 const app = express();
 
@@ -29,18 +23,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/',(req, res) =>{ res.send('it is working!') })
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
 app.post('/signin',signin.handleSignin(db, bcrypt));//Advanced function but same
 app.post('/register',(req,res) => { register.handleRegister(req, res, db, bcrypt) });
 app.get('/profile/:id',(req,res) => { profile.handleProfileGet(req, res, db) });
