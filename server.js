cb8057babf8bcb145042fb09c892797c89057dbf
Sumit <sumit.sth54@gmail.com>
2020-04-28 18:51:13 +0545

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db=knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-cubic-55087',
-    user : 'postgres',
-    password : 'toor',
-    database : 'facedetection'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
